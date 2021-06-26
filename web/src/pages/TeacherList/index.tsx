1d7a3cf07bb8685adce68f6c5718d88e98caffed
Hyago <hyagoassis@ice.ufjf.br>
2021-06-26 17:51:02 -0300

    Adcionando aula 3

--- web/src/pages/TeacherList/index.tsx
+++ web/src/pages/TeacherList/index.tsx
@@ -1,8 +1,10 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
+import Input from "../../components/Input";
 import PageHeader from '../../components/PageHeader';
 import TeacherItem from "../../components/TeacherItem";
+import Select from "../../components/Select";
 
 import './styles.css';
 
@@ -11,30 +13,42 @@ function TeacherList() {
     <div id="page-teacher-list" className="container">
       <PageHeader title="Estes são os proffys disponíveis.">
         <form id="search-teachers">
-
-          <div className="input-block">
-            <label htmlFor="subject">Matéria</label>
-            <input type="text" id="subject" />
-          </div>
-
-          <div className="input-block">
-            <label htmlFor="week_day">Dia da Semana</label>
-            <input type="text" id="week_day" />
-          </div>
-
-          <div className="input-block">
-            <label htmlFor="time">Hora</label>
-            <input type="text" id="time" />
-          </div>
+          <Select
+            name="subject"
+            label="Matéria"
+            options={[
+              { value: 'Artes', label: 'Artes' },
+              { value: 'Geografia', label: 'Geografia' },
+              { value: 'Matemática', label: 'Matemática' },
+              { value: 'Educação Física', label: 'Educação Física' },
+              { value: 'Biologia', label: 'Biologia' },
+              { value: 'História', label: 'História' },
+              { value: 'Português', label: 'Português' }
+            ]}
+          />
+          <Select
+            name="week_day"
+            label="Dia da Semana"
+            options={[
+              { value: '0', label: 'Domingo' },
+              { value: '1', label: 'Segunda-Feira' },
+              { value: '2', label: 'Terça-Feira' },
+              { value: '3', label: 'Quarta-Feira' },
+              { value: '4', label: 'Quinta-Feira' },
+              { value: '5', label: 'Sexta-Feira' },
+              { value: '6', label: 'Sábado' }
+            ]}
+          />
+          <Input type="time" name="time" label="Hora" />
 
         </form>
       </PageHeader>
 
       <main>
-        <TeacherItem/>
-        <TeacherItem/>
-        <TeacherItem/>
-        <TeacherItem/>
+        <TeacherItem />
+        <TeacherItem />
+        <TeacherItem />
+        <TeacherItem />
       </main>
 
     </div>
